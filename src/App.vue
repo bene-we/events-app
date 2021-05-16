703afda4a392d5e3af42055f00230dcd8683597c
Bene Weber <bene-we@e.mail.de>
2021-05-16 21:02:17 +0200

    Setup corporate design customizations

--- src/App.vue
+++ src/App.vue
@@ -18,11 +18,13 @@ export default class App extends Vue { }
 </script>
 
 <style lang="scss">
+html {
+  overflow-y: auto !important;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 </style>
