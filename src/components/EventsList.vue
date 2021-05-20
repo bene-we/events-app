2560ab77c47d305ba2d9c74e8b20f38f0fb95720
Bene Weber <bene-we@e.mail.de>
2021-05-20 11:25:00 +0200

    Add firebase integration
    Use firestore to store the event data.

--- src/components/EventsList.vue
+++ src/components/EventsList.vue
@@ -2,16 +2,18 @@
   <div class="events container mb-6 pb-6">
     <h2 class="subtitle is-3 mb-6">Check out our upcoming events</h2>
     <div class="columns is-multiline">
-      <div
-        v-for="event in events"
-        :event="event"
-        :key="event.id"
-        class="column is-one-quarter"
-      >
-        <router-link :to="'/event/' + event.id">
-          <EventCard :event="event" />
-        </router-link>
-      </div>
+      <template v-if="!loading">
+        <div
+          v-for="event in events"
+          :event="event"
+          :key="event.id"
+          class="column is-one-quarter"
+        >
+          <router-link :to="'/event/' + event.id">
+            <EventCard :event="event" />
+          </router-link>
+        </div>
+      </template>
     </div>
   </div>
 </template>
@@ -19,6 +21,8 @@
 <script lang='ts'>
 import { Component, Vue } from 'vue-property-decorator'
 import EventCard from '@/components/EventCard.vue'
+import { db } from '@/firebase'
+import { Event } from '@/models/event.model'
 
 @Component({
   components: {
@@ -26,37 +30,16 @@ import EventCard from '@/components/EventCard.vue'
   }
 })
 export default class EventsList extends Vue {
-  private event = {}
-  private events = [
-    {
-      id: 1,
-      name: 'Charity Ball',
-      category: 'Fundraising',
-      description:
-        'Spend an elegant night of dinner and dancing with us as we raise money for for our new rescue farm.',
-      featuredImage: 'https://placekitten.com/500/500',
-      images: [
-        'https://placekitten.com/500/500',
-        'https://placekitten.com/500/500',
-        'https://placekitten.com/500/500',
-      ],
-      location: '1234 Fancy Ave',
-      date: '12-25-2019',
-      time: '11:30',
-    },
-    {
-      id: 2,
-      name: 'Rescue Center Goods Drive',
-      category: 'Adoptions',
-      description:
-        'Come to our donation drive to help us replenish our stock of pet food, toys, bedding, etc. We will have live bands, games, food trucks, and much more.',
-      featuredImage: 'https://placekitten.com/g/500/500',
-      images: ['https://placekitten.com/500/500'],
-      location: '1234 Dog Alley',
-      date: '11-21-2019',
-      time: '12:00',
-    },
-  ]
+
+  private loading = false
+  private events: Event[] = []
+
+  async created (): Promise<void> {
+    this.loading = true
+    this.events = await db.getEvents()
+    console.log(this.events)
+    this.loading = false
+  }
 }
 </script>
 
